31b0fcce1bf4416cf5a362e10f026feaecfbb0de
Showrin <showrinece15@gmail.com>
2020-08-16 23:36:45 +0600

    Add styles to console texts using chalk

--- test/testingLibrary.js
+++ test/testingLibrary.js
@@ -1,11 +1,16 @@
+const chalk = require("chalk");
+
 const test = (testDescription, testFunction, expectedValue) => {
   const receivedValue = testFunction;
 
   if (receivedValue === expectedValue) {
-    console.log(`✔  ${testDescription}`);
+    console.log(chalk.green(`✔  ${testDescription}`));
   } else {
-    console.log(`✘  ${testDescription}`);
-    console.log(`\tExpected: ${expectedValue} \n\tReceived: ${receivedValue}`);
+    console.log(chalk.red(`✘  ${testDescription}`));
+    console.log(
+      chalk.green(`\tExpected: ${expectedValue} \n`),
+      chalk.red(`\tReceived: ${receivedValue}`)
+    );
   }
 
   return null;
